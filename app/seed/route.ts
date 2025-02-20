d81c59aa3ab816bd7ad23481faacec1915301b54
Jdthecreator <jetlyfejae@gmail.com>
2025-02-20 07:29:51 -0500

    new commit

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcryptjs from 'bcryptjs';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
@@ -17,7 +17,7 @@ async function seedUsers() {
 
   const insertedUsers = await Promise.all(
     users.map(async (user) => {
-      const hashedPassword = await bcrypt.hash(user.password, 10);
+      const hashedPassword = await bcryptjs.hash(user.password, 10);
       return sql`
         INSERT INTO users (id, name, email, password)
         VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
@@ -60,6 +60,8 @@ async function seedCustomers() {
 
   await sql`
     CREATE TABLE IF NOT EXISTS customers (
+
+    3
       id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
       name VARCHAR(255) NOT NULL,
       email VARCHAR(255) NOT NULL,
